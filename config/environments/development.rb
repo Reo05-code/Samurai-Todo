788aef3221ea5526ee8ee67aea63c800065391be
Reo05-code <t.asaishi05@gmai.com>
2025-10-06 02:21:00 +0900

    finish 2-3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: "localhost", port: ENV.fetch("PORT", 3000) }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -58,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # devise の設定
+ config.action_mailer.default_url_options = { host: "localhost", port: ENV.fetch("PORT", 3000)}
 end
